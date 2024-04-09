31014d2d97244a09b409292fb3fde52d044918bb
Tina12Jumde <158752623+Tina12Jumde@users.noreply.github.com>
2024-04-09 10:17:33 -0400

    work on js script

---
+++ script.js
@@ -0,0 +1,649 @@
+//{
+    /* Arithmatic operation */
+
+//    {
+  //      let a=prompt("enter the value of a");
+    //    console.log(typeof(a));
+      //  console.log(a);
+//
+  //      a=Number(a)
+  //      console.log(typeof(a))
+//
+  //      let b=prompt("enter the value of b");
+    //    console.log(typeof(b));
+      //  console.log(b);
+
+//        b=Number(b)
+  //      console.log(typeof(b));
+    //    c=a*b;
+
+//        console.log(c);
+  //  }
+
+    /* if-else statements */
+
+    //{
+  //      let age=prompt("enter your age")
+
+//        if(age>18){
+  //          console.log("you can run")
+    //    }  else{
+      //      console.log("sorry! you cannot run")
+        //}
+        
+    //}
+
+
+    /* nested if-else */
+
+    
+    //{
+        //let a=prompt("enter the value of a");
+     //   let b=prompt("enter the value of b");
+       // let c=prompt("enter the value of c");
+
+    //    if(a>b)
+      //  {
+        //    if(a>c)
+          //  {
+            //    console.log("a is the greater than b and c");
+
+      //      }
+          //  else
+        //    {
+            //    console.log("c is greater than a and b");
+          //  }
+      //  }
+      //  else{
+       //     if(b>c)
+            //{
+        //        console.log("b is greater than a and c");
+        //    }
+          //  else
+          //  {
+            //    console.log("c is greater than a and b");
+            //}
+      //  }
+   // }
+
+
+     /* Multipilcation operation */
+
+
+     
+     //   let a=20;
+       // let b=5;
+     //   let choice=prompt("enter your choice");
+       // choice=Number(choice)
+
+         //switch(choice)
+        
+           // case 1:console.log("addition is", a+b)
+         //   break;
+           // case 2:console.log("substraction is", a-b)
+          //  break;
+            //case 3:console.log("multiplication is", a*b)
+          //  break;
+            //case 4:console.log("division is", a/b)
+          //  break;
+            //case 5:console.log("modulus is, a%b")
+         //   break;
+           // default:console.log("not a valid choice")
+     //   }
+   //  }
+
+
+//  }
+
+
+/* Factorials */
+
+// wap in  to find factor in number //
+
+    //  let number=10
+    // let org=number
+    //  let Factorials=1
+
+      // while(number !=0){
+    //    fact = fact*number
+
+      //  number--
+    //  }
+
+      //console.log('fact of $(org) is $(fact)!')
+
+       /*  Polindrome number/strings */
+
+
+    //wap in js to reverse a numbers //
+
+   // let num = 2345 //5432
+   // let org = num
+    //let rev = 0
+   // let d;
+   // while(){
+       // d = num % 10
+       // rev = rev * 10 + d
+      //  num = parseInt(num/10)
+  //  }
+
+   // console.log(rev)
+   // if(org == rev){
+      //  console.log('$(num) is a polindrome !')
+  //  } 
+  //  else{
+   //     console.log('$(org) is not a polindromre !')
+   // }
+
+    /* sum of digits */
+
+    //digits//
+
+   // let num =123
+   // let org =num
+   // let d;
+  //  let sum = 0
+
+  //  while(num !=0){
+   //     d = num % 10
+     //   sum = sum + d //3+0=3, 3+2=5, 5+1=6
+       // num = parseInt(num / 10)
+ //   }
+  //  console.log('The sum of digits of $(org) is $(sum)!')
+
+    /* Armstrong number */
+
+    //sum of cube of digits is equal to original number //
+
+ //   let num = 123
+   // let org = num
+ //   let d;
+ //   let sum = 0
+
+   // while(console.log(num.length)){
+      //  d = num % 10
+      //  sum = sum + (d*d*d)
+      //  num = parseInt(num / 10)
+//}
+
+   // console.log  sum
+    
+  //  if(org == sum){
+//  let digits = 0
+   // let d;
+
+  //  while (num !=0){
+  //      d = num % 10
+    //    if(d){
+      //      digits ++
+        //  }
+     //   num = parseInt(num / 10)
+  //  }
+    // console.log(digits)
+
+    // wap to find wheather of a number //
+
+ //   let num = 12345
+   // let org = num
+  //  let digits = 0
+  //  let d;
+ //   let sum = 0
+
+    // while (num! = 0){
+     //   d = num % 10
+
+      //  if (d){
+      //      digits++
+      //  } num = parseInt(num % 10)
+      //  console.log('$(org) has $(digits) digits!')
+
+      //  num + org 
+        //num + org reset with original value //
+        // console.log(org)//
+      //  while(num!=0){
+      //      let d = num%10
+      //      let sum = sum + Math.pow(d.digits)
+       //      num = parseInt(num / 10)
+      //  }
+     //}
+
+
+     //{
+  //  let sum=0;
+   // let n=15;
+  //  for(
+  //     let i=1;i<=10;i++ ){
+    //        sum=sum+i;
+    //    }
+    //    console.log("sum=",sum);
+//}
+
+//{
+ //  let num=9;
+   //let multi=0;
+ //  for(
+  //  let i=1;i<=9;i++){
+   //     multi=multi*i;
+ //   }
+ //   console.log("multi=",multi);
+ //   if(num){
+ //       console.log('${num} is a armstong number !')
+ //   }else{
+ //       console.log('${num} is not a armstong number !')
+ //   }  
+//}
+
+//wap to find numbers of digits in a number 
+
+//{
+   // let num=456373542;
+   // let d;
+   // let digits=0;
+  //  for (i=1;i<=num;i++){
+   //     if(d){
+   //         digits++
+     //   }
+  //      num = parseInt(num/10)
+   // }
+   //  console.log("i=",i)
+//}
+
+  // Even numbers
+  //for(let i=0;i<10;i++){
+   // if(i%4===0){
+     //   console.log("i=",i)
+   // }
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,213 @@
+//array
+
+//for of
+
+
+//let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+//console.log(numbers.length)
+
+
+//for(let number of numbers){
+  //console.log(number)
+//}
+
+//let matrix=[11,12,13,14,15,16,17,18,19,20,21,22]
+//let arraymatrix=[
+  //  [11,12,13,],
+    //[14,15,16],
+  //  [17,18,19],
+  //  [20,21,22]
+//]
+
+//for(let row of matrix){
+  //  for(let column of matrix){
+    //    console.log(column)
+  //  }
+//}
+
+
+//for each
+
+//let students =[1,2,3,4,5,];
+
+//accepts callback function
+
+//students.forEach((element,index)=>{
+
+//})
+
+// Array.forEach(()=>{
+    
+// })
+
+//students.forEach((elements,index)=>{
+  //  console.log(`from forEach method:${elements}`);
+  //  console.log(`from forEach method:${index}`)
+    
+//})
+
+
+
+let numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+//let odd=[]
+//let even=[]
+
+//numbers.forEach((element)=>{
+ //   if(element%2==0){
+ //       even.push(element)
+//    }else{
+ //       odd.push(element)
+  //  }
+//})
+
+//console.log(odd)
+//console.log(even)
+
+
+//MAP LOOP
+
+//let number=[1,2,3,4,5,6,7,8,9]
+
+//let DisplayElement=(element,index)=>{
+ //   console.log(element)
+ //   console.log(index)
+//}
+
+//number.map(DisplayElement)
+
+//(callBack function)
+
+
+//ARRAY OF OBJECTS
+
+let students=[
+{name:"Tina",age:21,roll:1,class:"FSD"},
+{name:"Piyush",age:21,roll:2,class:"FSD"},
+{name:"Ruchi",age:20,roll:3,class:"FSD"},
+{name:"Srushti",age:19,roll:4,class:"FSD"},
+{name:"Deepak",age:20,roll:5,class:"FSD"},
+{name:"Ajay",age:25,roll:6,class:"FSD"},
+{name:"Rohit",age:24,roll:7,class:"FSD"},
+{name:"Mithansh",age:15,roll:8,class:"FSD"},
+{name:"Kartik",age:18,roll:9,class:"FSD"},
+{name:"Abhishek",age:24,roll:10,class:"FSD"},
+{name:"Kunal",age:23,roll:11,class:"FSD"},
+{name:"Harsh",age:19,roll:12,class:"FSD"},
+{name:"Yash",age:25,roll:13,class:"FSD"},
+{name:"Hardik",age:19,roll:14,class:"FSD"}
+]
+
+//for of
+
+//for(let student of students){
+ //   console.log(student)
+    //}
+
+
+
+//forEach
+
+
+//students.forEach((student,and,index)=>{
+ //   console.log(student)
+ //   console.log(and)
+ //   console.log(index)
+
+//})
+
+
+//let displayStudents=(student)=>{
+ //   console.log("from map")
+//    console.log(student)
+//}
+
+
+// students.map(displayStudents)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
